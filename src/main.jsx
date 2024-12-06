d19d416df08652af353fc2be31dc7c13d37554c9
zel0g0n <alisheroromov473@gmail.com>
2024-12-06 13:13:24 +0500

    added

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './style/style.scss'
+import App from './components/app/app'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
